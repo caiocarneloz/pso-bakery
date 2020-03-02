b625a62a8ad8930c7e5436f35f23b3327c4d8156
Caio Carneloz <caiocarneloz@users.noreply.github.com>
2020-03-02 13:00:09 -0300

    Update problem.py

--- problem.py
+++ problem.py
@@ -11,29 +11,23 @@ class BakeryProblem:
         self.products_price        = products_price
         self.products_consumption  = products_consumption 
         
-        self.mins                  = []
-        self.maxs                  = []
+        mins = []
+        maxs = []
 
         for product in self.products_price.keys():
-            self.mins.append(0)
-            self.maxs.append(np.min(self.available_ingredients/self.products_consumption[product]))
+            mins.append(0)
+            maxs.append(np.min(self.available_ingredients/self.products_consumption[product]))
             
-        self.bounder               = ec.Bounder(self.mins, self.maxs)
-
-    def fitness_func(self, candidate):
-        
-        metric = 0
-
-        return metric        
+        self.bounder               = ec.Bounder(lower_bound=mins, upper_bound=maxs)
     
     def generator(self, random, args):
+        
+        particle = []
 
-            individual = []
+        for i in range(0,len(self.products_price)):
+            particle.append(random.uniform(self.bounder.lower_bound[i], self.bounder.upper_bound[i]))
 
-            for i in range(0,len(self.products_price)):
-                individual.append(random.uniform(self.mins[i], self.maxs[i]))
-
-            return individual
+        return particle
 
     def evaluator(self, candidates, args):
 
@@ -42,14 +36,15 @@ class BakeryProblem:
             for c in candidates:
                 
                 ingredient_use = np.zeros(shape=(len(self.available_ingredients)))
+                
                 for index in range(0,len(c)):
                     ingredient_use += int(c[index])*self.products_consumption[list(self.products_consumption.keys())[index]]
                     
                 ingredient_use = self.available_ingredients - ingredient_use
                 
                 metric = sum(ingredient_use[ingredient_use < 0])*sum(c)
-                for i in range(0, len(c)):
-                    metric += int(c[i])*self.products_price[list(self.products_price.keys())[i]]
+                
+                metric += sum(c*np.array(list(self.products_price.values()), dtype=float))
                 
                 fitness.append(metric)
 
